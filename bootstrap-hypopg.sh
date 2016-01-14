c84e7a3131edac522b06fa509ce5e4e74fef256b
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-01-14 15:11:28 -0500

    removed yum lines

--- bootstrap-hypopg.sh
+++ bootstrap-hypopg.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-## make necessary changes when performing the build in the admin terminal
-
-sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install yum localinstall http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum -y install postgresql9*
-sudo yum -y install java-1.7.0*
-sudo yum -y install tree
-sudo yum -y install vim
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
